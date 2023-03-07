02468285c40a661b0341f3b4ef6074c27e0c1f83
Thulasi-Dass-N <thulasi.d@bahwancybertek.com>
2023-03-07 19:40:35 +0530

    app theme , back buttton

--- App.js
+++ App.js
@@ -4,35 +4,35 @@ import {View, Text, StyleSheet} from "react-native";
 import AppInput from "./src/components/atoms/AppInput";
 
 const App = () => {
-	return (
-		<View style={{flex: 1, justifyContent: "center"}}>
+  return (
+    <View style={{flex: 1, justifyContent: "center"}}>
 			
-			<AppInput/>
-			<Text>Hello</Text>
-		</View>
-	);
+      <AppInput/>
+      <Text styles={styles.text}>Hello</Text>
+    </View>
+  );
 };
 
 export default App;
 const styles = StyleSheet.create({
-	container: {
-		height: 40,
-		borderRadius: 45,
-		backgroundColor: "black",
-		justifyContent: "center",
-		paddingHorizontal: 30,
-		alignItems: "center",
-		alignSelf: "center",
-		borderWidth:1,
-		borderColor:"green"
+  container: {
+    height: 40,
+    borderRadius: 45,
+    backgroundColor: "black",
+    justifyContent: "center",
+    paddingHorizontal: 30,
+    alignItems: "center",
+    alignSelf: "center",
+    borderWidth:1,
+    borderColor:"green"
 	
-	},
-	text: {
-		color: "white",
-		fontFamily: "ClashDisplay-Regular",
-		fontSize: 15,
-		lineHeight: 20,
-		fontWeight: "400",
-	},
+  },
+  text: {
+    color: "white",
+    fontFamily: "ClashDisplay-Regular",
+    fontSize: 15,
+    lineHeight: 20,
+    fontWeight: "400",
+  },
 
 });
